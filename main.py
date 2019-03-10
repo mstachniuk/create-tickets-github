8d0d62c19e2e4d8664057a024226e193513fcf07
Marcin Stachniuk <marcin.stachniuk@collibra.com>
2019-03-10 23:09:28 +0100

    Solve tickets

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import sys
 import requests
+from shutil import copyfile, rmtree
 
 # This script create tickets on GitHub. It's useful for Shipkit workshop:
 # https://github.com/mstachniuk/shipkit-workshop
@@ -22,10 +23,61 @@ def createTickets(repo, token):
     ownerRepo = repo[begin:]
     print 'found ' + ownerRepo
     url = 'https://api.github.com/repos/' + ownerRepo + '/issues'
-    createTicket(url, token, "Floating point support", "Please add a support for floating point opperations. \\nPlease add 'new feature' label to the PR for this issue. \\nThis feature should be implemented by repo owner.")
-    createTicket(url, token, "Unit tests for MathUtils are missing", "Please add unit tests for MathUtils. \\nPlease add 'quality' label to the PR for this issue. \\nThis feature should be implemented by NOT a repo owner.")
+    createTicket(url, token, "Add long point support", "Please add a support for long isTriangle.")
+    createTicket(url, token, "Add float support", "Please add a support for float isTriangle.")
+    createTicket(url, token, "Add BigInteger support", "Please add a support for BigInteger isTriangle.")
+    createTicket(url, token, "Add BigDecimal point support", "Please add a support for BigDecimal isTriangle.")
+    # createTicket(url, token, "Unit tests for MathUtils are missing", "Please add unit tests for MathUtils.")
     createTicket(url, token, "Readme file is missing", "Please add a Readme file with project description.")
     createTicket(url, token, "Javadoc for MathUtils class is missing", "Please add Javadoc for MathUtils class.")
+    
+def cloneRepo(repo):
+    rmtree('/tmp/shipkit-w')
+    os.chdir('/tmp')
+    subprocess.check_output('git clone ' + repo + ' shipkit-w', shell=True)
+    os.chdir('/tmp/shipkit-w')
+
+def changeGitEmail():
+    subprocess.check_output('git config user.email mstachniuk@gmail.com', shell=True)
+
+def addLongSupport(repo, scriptDir):
+    subprocess.check_output('git checkout -b long-support', shell=True)
+    copyfile(scriptDir + '/MathUtils.java.v1', '/tmp/shipkit-w/src/main/java/com/example/shipkitworkshop/MathUtils.java')
+    subprocess.check_output('git add -A', shell=True)
+    subprocess.check_output('git commit -m "Add long support"', shell=True)
+    subprocess.check_output('git push --set-upstream origin long-support', shell=True)
+
+def addFloatSupport(repo, scriptDir):
+    subprocess.check_output('git checkout -b float-support', shell=True)
+    copyfile(scriptDir + '/MathUtils.java.v2', '/tmp/shipkit-w/src/main/java/com/example/shipkitworkshop/MathUtils.java')
+    subprocess.check_output('git add -A', shell=True)
+    subprocess.check_output('git commit -m "Add float support"', shell=True)
+    subprocess.check_output('git push --set-upstream origin float-support', shell=True)
+
+def addBigDecimalSupport(repo, scriptDir):
+    subprocess.check_output('git checkout -b big-decimal-support', shell=True)
+    copyfile(scriptDir + '/MathUtils.java.v3', '/tmp/shipkit-w/src/main/java/com/example/shipkitworkshop/MathUtils.java')
+    subprocess.check_output('git add -A', shell=True)
+    subprocess.check_output('git commit -m "Add BigDecimal support"', shell=True)
+    subprocess.check_output('git push --set-upstream origin big-decimal-support', shell=True)
+
+def addBigIntegerSupport(repo, scriptDir):
+    subprocess.check_output('git checkout -b big-integer-support', shell=True)
+    copyfile(scriptDir + '/MathUtils.java.v4', '/tmp/shipkit-w/src/main/java/com/example/shipkitworkshop/MathUtils.java')
+    subprocess.check_output('git add -A', shell=True)
+    subprocess.check_output('git commit -m "Add BigInteger support"', shell=True)
+    subprocess.check_output('git push --set-upstream origin big-integer-support', shell=True)
+
+def createThings(repo, token):
+    createTickets(repo, token)
+    scriptDir = os.getcwd()
+    print 'script dir: ' + scriptDir
+    cloneRepo(repo)
+    changeGitEmail()
+    addLongSupport(repo, scriptDir)
+    addFloatSupport(repo, scriptDir)
+    addBigDecimalSupport(repo, scriptDir)
+    addBigIntegerSupport(repo, scriptDir)
     
 
 if __name__ == "__main__":
@@ -41,6 +93,6 @@ if __name__ == "__main__":
 
         for repo in repos:
             print repo
-            createTickets(repo, token)
+            createThings(repo, token)
             
             
